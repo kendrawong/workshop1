a8f6a367313dbef62e7dec5e6bde4deb0c946e51
kendrawong <kendra.m.wong@gmail.com>
2018-01-24 10:15:12 -0800

    Create Puzzle7.js

---
+++ Puzzle7.js
@@ -0,0 +1,26 @@
+function moveDown(){
+  while (getColor() != "red"){
+    down();
+  }
+}
+
+function moveUp(){
+  while (getColor() != "red"){
+    up();
+  }
+}
+
+moveDown();
+right();
+right();
+moveUp();
+right();
+right();
+moveDown();
+right();
+right();
+moveUp();
+right();
+right();
+moveDown();
+right();
