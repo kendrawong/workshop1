f4947e34ce77c4ffa14e886eb7b9603fbec3712d
kendrawong <kendra.m.wong@gmail.com>
2018-01-24 10:25:19 -0800

    Update Puzzle6.js

--- Puzzle6.js
+++ Puzzle6.js
@@ -1,19 +1,24 @@
-function colorBlock(){
-  if (getColor() == "red"){
-    up();
-    right();
-    right();
-    }
-  else if (getColor() == "blue"){
-    down();
-    right();
-    right();
-    }
- }
- 
 right();
-right();
-colorBlock();
-colorBlock();
-colorBlock();
-right();
+if (getColor() == "red"){
+  up(); 
+  up();
+  up();
+  }
+else if (getColor() == "blue"){
+  down();
+  down();
+  down();
+  }
+var centerColor = getColor()
+if (getColor() == "red"){
+  left();
+  }
+else if (getColor() == "blue"){
+  right();
+  }
+if (centerColor == "red"){
+  up();
+  }
+else if (centerColor == "blue"){
+  down();
+  }
