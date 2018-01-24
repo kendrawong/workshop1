f5717b8f6449ae14719ee4d078aab620c7a97243
kendrawong <kendra.m.wong@gmail.com>
2018-01-24 10:11:21 -0800

    Create Puzzle4.js

---
+++ Puzzle4.js
@@ -0,0 +1,16 @@
+function openAndObtain(){
+  right();
+  var currentColor = getColor()
+  down();
+  setColor(currentColor)
+  right();
+  right();
+  }
+  
+down();
+down();
+down();
+down();
+openAndObtain();
+up();
+openAndObtain();
