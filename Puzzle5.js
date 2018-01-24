46c0cea3082b60ed5a454b280a4467f22f18f481
kendrawong <kendra.m.wong@gmail.com>
2018-01-24 10:26:42 -0800

    Create Puzzle5.js

---
+++ Puzzle5.js
@@ -0,0 +1,19 @@
+function colorBlock(){
+  if (getColor() == "red"){
+    up();
+    right();
+    right();
+    }
+  else if (getColor() == "blue"){
+    down();
+    right();
+    right();
+    }
+ }
+ 
+right();
+right();
+colorBlock();
+colorBlock();
+colorBlock();
+right();
